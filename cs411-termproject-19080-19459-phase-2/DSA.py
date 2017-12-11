e228200fcfa9d2e7c5ea2f7ca520426d7e1afb1b
Berkan Teber <berkanteber@sabanciuniv.edu>
2017-12-11 18:38:14 +0300

    Phase 2 - Step 4

--- cs411-termproject-19080-19459-phase-2/DSA.py
+++ cs411-termproject-19080-19459-phase-2/DSA.py
@@ -10,57 +10,62 @@
 # Program also generates the file DSA_params.txt which is the requested file                              ##
 ############################################################################################################
 
-import time
-from random import seed, randint
-
 import MillerRabinTestByErkaySavas
 
-def main():
-    seed("19080-19459")
-
-    smallbit = 256
-    largebit = 2048
-
-    params = open("DSA_params.txt", "w")
-
-    ctrq = 1
+def DL_Param_Generator(small_bound, large_bound):
     while True:
         q = randint(2**(smallbit-1), 2**smallbit - 1)
-
         if MillerRabinTestByErkaySavas.PrimalityTest(q, 10) == 1:
-            params.write(str(q) + "\n")
-            print "q has been found in " + str(ctrq) + " steps."
             break
 
-        ctrq += 1
-
-    ctrp = 1
     while True:
         p = q * randint(2**(largebit-smallbit-1), 2**(largebit-smallbit)) + 1
-
         if MillerRabinTestByErkaySavas.PrimalityTest(p, 10) == 1:
-            params.write(str(p) + "\n")
-            print "p has been found in " + str(ctrp) + " steps."
             break
 
-        ctrp += 1
-
-    ctrg = 1
     while True:
         alpha = randint(0, p - 1)
-
         g = pow(alpha, (p-1) / q, p)
         if g != 1:
-            params.write(str(g) + "\n")
-            print "g has been found in " + str(ctrg) + " steps."
             break
 
-        ctrg += 1
+    return q, p, g
 
-    params.close()
+def KeyGen(p, q, g):
+    alpha = randint(1, q - 1)
+    beta = pow(g, alpha, p)
 
-if __name__ == "__main__":
-    start_time = time.time()
-    main()
-    end_time = time.time()
-    print "Execution Time: %s seconds" % (end_time - start_time)
+    return (alpha, beta)
+
+def SignGen(p, q, g, alpha, beta):
+    string = "*** Bitcoin transaction ***\n"
+
+    serial = randint(0, 2**128 - 1)
+    string += "Serial number: " + str(serial) + "\n"
+
+    payer = hex(randint(0, 2**40 - 1))[2:].upper()
+    string += "Payer: " + payer + "\n"
+
+    payee = hex(randint(0, 2**40 - 1))[2:].upper()
+    string += "Payee: " + payee + "\n"
+
+    amount = randint(1, 1000)
+    string += "Amount: " + str(amount) + " Satoshi" + "\n"
+
+    string += "p: " + str(p) + "\n"
+    string += "q: " + str(q) + "\n"
+    string += "g: " + str(g) + "\n"
+
+    string += "Public Key (beta): " + str(beta) + "\n"
+
+    # compute signature
+
+    h = int(hashlib.sha3_256(string).hexdigest(), 16)
+    h = h % q
+
+    k = randint(1, q - 1)
+    r = pow(g, k, p)
+
+    s = (alpha * r + k * h) % q
+
+    return (r, s)
---
+++ cs411-termproject-19080-19459-phase-2/TXGen.py
@@ -0,0 +1,47 @@
+############################################################################################################
+# Project Group:                                                                                          ##
+# 19080 - Berkan Teber                                                                                    ##
+# 19459 - Arda Olmezsoy                                                                                   ##
+############################################################################################################
+# Important Note:                                                                                         ##
+# This program has been written and executed in UNIX.                                                     ##
+############################################################################################################
+# Program prints out the execution time, which is approximately 25 minutes, as standard output.           ##
+# Program also generates the file SignatureTransaction.txt, which is the requested file.                  ##
+############################################################################################################
+
+def GenSingleTx:
+    string = "*** Bitcoin transaction ***\n"
+
+    serial = randint(0, 2**128 - 1)
+    string += "Serial number: " + str(serial) + "\n"
+
+    payer = hex(randint(0, 2**40 - 1))[2:].upper()
+    string += "Payer: " + payer + "\n"
+
+    payee = hex(randint(0, 2**40 - 1))[2:].upper()
+    string += "Payee: " + payee + "\n"
+
+    amount = randint(1, 1000)
+    string += "Amount: " + str(amount) + " Satoshi" + "\n"
+
+    string += "p: " + str(p) + "\n"
+    string += "q: " + str(q) + "\n"
+    string += "g: " + str(g) + "\n"
+
+    string += "Public Key (beta): " + str(beta) + "\n"
+
+    # compute signature
+
+    h = int(hashlib.sha3_256(string).hexdigest(), 16)
+    h = h % q
+
+    k = randint(1, q - 1)
+    r = pow(g, k, p)
+
+    s = (alpha * r + k * h) % q
+
+    string += "Signature (r): " + str(r) + "\n"
+    string += "Signature (s): " + str(s) + "\n"
+
+    return string
